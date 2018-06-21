e730904d7b86b4c15e8c5f83b1dd64451c0e145d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-21 11:49:43 -0700

    Added list slicing, slicing assignment, and index/slicing deletion functions

--- list_operations.py
+++ list_operations.py
@@ -84,7 +84,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[0:3]
 
 
 def last_five(input_list):
@@ -97,7 +97,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -110,7 +110,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -123,7 +123,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -139,7 +139,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -153,8 +153,8 @@ def replace_head(input_list):
     True
 
     """
-
-    pass
+    input_list[0] = 42
+    return
 
 
 def replace_third_and_last(input_list):
@@ -168,8 +168,9 @@ def replace_third_and_last(input_list):
     True
 
     """
-
-    pass
+    input_list[2] = 37
+    input_list[-1] = 37
+    return
 
 
 def replace_middle(input_list):
@@ -187,8 +188,8 @@ def replace_middle(input_list):
     True
 
     """
-
-    pass
+    input_list[2:-2] = 42, 37
+    return
 
 
 def delete_third_and_seventh(input_list):
@@ -202,8 +203,9 @@ def delete_third_and_seventh(input_list):
     True
 
     """
-
-    pass
+    del input_list[2]
+    del input_list[5]
+    return
 
 
 def delete_middle(input_list):
@@ -219,8 +221,8 @@ def delete_middle(input_list):
     True
 
     """
-
-    pass
+    del input_list[2:-2]
+    return
 
 
 ##############################################################################
